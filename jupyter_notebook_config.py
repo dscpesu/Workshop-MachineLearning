1f119ce9053aaa33863b5ef3c54054b1d241759c
Sumanth V Rao <sumanthvrao@gmail.com>
2019-03-25 23:22:10 +0530

    v2: Dockerfile and docker-compose file added.

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,12 @@
+import os
+from IPython.lib import passwd
+
+# Set Jupyter Notebook's IP & port.
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = int(os.getenv('PORT', 8888))
+c.NotebookApp.open_browser = False
+
+# Set a password if PASSWORD is set in the environment
+if 'PASSWORD' in os.environ:
+    c.NotebookApp.password = passwd(os.environ['PASSWORD'])
+del os.environ['PASSWORD']
